601e3c99e97a80d8f92d2fd1e4ad75e810eb2174
Manuel Ulate <fr3zs09@gmail.com>
2024-11-24 16:59:40 -0600

    feat(general):middleware implemented on server

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import { envs } from './config';
+import { GithubController } from './presentation/github/controller';
+import { GithubSha256Middleware } from './presentation/middlewares/github-sha256.middleware';
 
 (()=>{
 
@@ -10,8 +12,12 @@ import { envs } from './config';
 function main () {
 
   const app = express();
-
+  
   app.use( express.json() );
+  app.use( GithubSha256Middleware.verifySignature );
+  
+  const controller = new GithubController();
+  app.post('/api/github', controller.webhookHandler );
 
   app.listen( envs.PORT, () => {
     console.log(`Server listening on port ${envs.PORT}`);
